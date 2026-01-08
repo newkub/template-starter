2bc5dba4963a1fb04ae784c4544d7d9df0f975b5
Veerapong <newkubise@gmail.com>
2026-01-09 04:34:15 +0700

    chore(templates): remove deprecated templates

--- templates/nuxt/server/api/plugins/[id]/activate.post.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(async (event) => {
-	const id = getRouterParam(event, "id");
-	// TODO: Implement actual plugin activation logic
-	return { success: true, message: `Plugin ${id} activated` };
-});
--- templates/nuxt/server/api/plugins/[id]/deactivate.post.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(async (event) => {
-	const id = getRouterParam(event, "id");
-	// TODO: Implement actual plugin deactivation logic
-	return { success: true, message: `Plugin ${id} deactivated` };
-});
