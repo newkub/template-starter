d3e014fe1dae9b8713518811b4be8bea74dee084
newkub <newkubise@gmail.com>
2026-06-10 07:44:48 +0700

    fix: remove apps/* from workspaces and fix version specifiers

---
+++ TODO.md
@@ -0,0 +1,2 @@
+* ###### ที่ apps/template-cli เปลี่ยนเป็น cli และทำให้สมบูรณ์ และ separate of concern และ bun typecheck, run build และเพิ่ม test ด้วย run test ด้วย
+
--- apps/web/.output/public/_nuxt/D8VZqghm.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,x as r,y as n,$ as o}from"./CVU5KSwP.js";const s={};function a(e,c){return n(),r("div",null,[o(e.$slots,"default")])}const _=t(s,[["render",a]]);export{_ as default};
